68dfcb753e24d6de30ba648b747c1118741f685d
Marlon Andres Barriga Medina <marlon.barriga@example.com>
2025-11-08 14:52:24 -0500

    Actualizacion 12

---
+++ src/hoisting/index.js
@@ -0,0 +1,14 @@
+// var  nameDfog; //
+
+// console.log(nameDfog);
+// var nameDfog = 'Elmo';
+// console.log(nameDfog);
+
+// it  comment for variable is same as function
+
+nameOfDog();
+function nameOfDog() {
+    console.log(`El mejor perrito es ${elmo}`);
+}
+
+var elmo = 'Elmito ' ;
